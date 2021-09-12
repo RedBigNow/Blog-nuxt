f4d823674bf6892d5505383ecda6406f9e2792bf
Nikita <nikita_sokolov98@mail.ru>
2021-09-12 17:12:15 +0300

    added posts and contact form on homepage

---
+++ components/Blog/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="posts__wrapper">
+        <div class="container">
+            <div class="post-list">
+                <postPreview 
+                    v-for="post in posts" :key="post.id"
+                    :post="post"
+                />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import postPreview from '@/components/Blog/PostPreview.vue'
+
+export default {
+    components: { postPreview },
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.post-list {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
---
+++ components/Contacts.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="contacts">
+        <div class="container">
+            <h2 class="title">Contact me!</h2>
+            <form @submit.prevent="onSubmit" class="contacts__form">
+                <label>Name:</label>
+                <input v-model="user.name" type="text">
+                <label>Email:</label>
+                <input v-model="user.email" type="text">
+                <label>Message:</label>
+                <textarea v-model="user.text"></textarea>
+                <div class="controls">
+                    <button class="btn btnWhite">Submit!</button>
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            user: {
+                name: '',
+                email: '',
+                text: ''
+            }
+        }
+    },
+    methods: {
+        onSubmit () {
+            console.log(this.user)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.contacts {
+    text-align: center;
+    background-color: #4b40e3;
+    color: #fff;
+
+    h2.title {
+        color: #fff;
+    }
+
+    .contacts__form {
+        max-width: 600px;
+        margin: 30px auto;
+    }
+
+    .controls {
+        margin: 30px 0;
+    }
+}
+</style>
